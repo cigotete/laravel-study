b57e3ea160d8adb7237cbfc574a25f43730baaac
Camilo Figueroa <cigotete@gmail.com>
2023-01-12 17:04:13 -0500

    Added old function to form fields at edit view, to show old values when error validation updates form.

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -14,11 +14,11 @@
     <form method="POST" action="{{ route('projects.update', $project) }}">
         @csrf @method('PATCH')
         <label for="">Title</label>
-        <input type="text" name="title" value="{{ $project->title }}"></br>
+        <input type="text" name="title" value="{{ old('title', $project->title) }}"></br>
         <label for="">Url</label>
-        <input type="text" name="url" value="{{ $project->url }}"></br>
+        <input type="text" name="url" value="{{ old('url', $project->url) }}"></br>
         <label for="">Description</label>
-        <textarea name="description">{{ $project->description }}</textarea></br>
+        <textarea name="description">{{ old('description', $project->description) }}</textarea></br>
         <button>Update project</button>
     </form>
 
