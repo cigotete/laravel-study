026d2430d93811e88dbeb6ca7fcd763328f089b9
Camilo Figueroa <cigotete@gmail.com>
2023-01-14 11:57:57 -0500

    Adding login and logout links to nav.

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -4,6 +4,17 @@
         <li class="{{ setActive('about') }}"><a href="{{ route('about') }}">About</a></li>
         <li class="{{ setActive('projects.*') }}"><a href="{{ route('projects.index') }}">Portfolio</a></li>
         <li class="{{ setActive('contact') }}"><a href="{{ route('contact') }}">Contact</a></li>
-        <li class="{{ setActive('test') }}"><a href="{{ route('test') }}">Test</a></li>
+        @guest
+            <li class="{{ setActive('login') }}"><a href="{{ route('login') }}">Login</a></li>
+        @else
+            <li class="{{ setActive('login') }}"><a class="dropdown-item" href="{{ route('logout') }}"
+                                                    onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a></li>
+        @endguest
     </ul>
 </nav>
+<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+    @csrf
+</form>
